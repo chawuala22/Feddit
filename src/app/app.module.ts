e83243ee16309cac9bd04f508b1bb841f9023439
Chawuala22 <mrojasgarcia22@gmail.com>
2020-12-16 03:41:23 -0500

    Avance login Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms'
+
+
+@NgModule({
+  declarations: [
+    AppComponent, NavbarComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
